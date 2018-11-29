efd7247d05ec2ef74a760c94a1a0a9bd563c2add
Dalya Eidda <dalia.eidda@gmail.com>
2018-11-29 13:42:18 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection) 
+if collection.length > 0 
+  while 
+yield(collection)
+"#{collection}"
+end 
+else 
+  "This block should not run!"
+end 
+
+collection 
 end
