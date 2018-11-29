a729d1a26a9ef0b4f0894a73fe018d7eb53f4ab9
Dalya Eidda <dalia.eidda@gmail.com>
2018-11-29 15:39:45 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+while collection.length > 0 
+1.times collection
+yield |word|
+word.length? 
+"#{word}"
+
+end 
+collection 
 end
